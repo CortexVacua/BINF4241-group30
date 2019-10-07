5520658a30b62b49c2fc5c4db58aa2039d55836f
MinhFu <45556687+MinhFu@users.noreply.github.com>
2019-10-07 09:18:56 +0200

    Added some comments mostly in Game.java

--- Snakes_and_Ladders/src/Gameboard.java
+++ Snakes_and_Ladders/src/Gameboard.java
@@ -9,17 +9,22 @@ public class Gameboard {
     static int board_size;
     static Square[] list_of_squares;
     public static void init() {
+        // initializes the board as a list
         System.out.print("Enter the board size: ");
         board_size = input.nextInt();
         list_of_squares = new Square[board_size];
+        // adds first and last square
         FirstSquare first = new FirstSquare();
         list_of_squares[0] = first;
         LastSquare last = new LastSquare();
         list_of_squares[board_size -1] = last;
+        //makes ladders at every fifth square. begins at the third square.
         for (int i = 3; i < board_size - 5; i+=5 ){
             Ladder ladder = new Ladder(i);
             list_of_squares[i-1] = ladder;
         }
+        // makes snakes at every sixth square. begins at the fifth square.
+        // if there is a ladder at the counted square, the next suqare will be a snake.
         for (int i = 5; i < board_size - 2; i+=6 ) {
             if (list_of_squares[i - 1] == null) {
                 Snake snake = new Snake(i);
@@ -29,12 +34,14 @@ public class Gameboard {
                 list_of_squares[i] = snake;
             }
         }
+        // fills the rest of the board with normal squares
         for (int i = 2; i < board_size; i++) {
             if (list_of_squares[i - 1] == null) {
                 Square square = new Square(i);
                 list_of_squares[i - 1] = square;
             }
         }
+        //initialize players and add them into a player list.
         System.out.print("Enter the number of players (between 2 and 4): ");
         int number_of_players = input.nextInt();
         for (int i = 1; i <= number_of_players; i++) {
--- Snakes_and_Ladders/src/Snake.java
+++ Snakes_and_Ladders/src/Snake.java
@@ -1,4 +1,5 @@
 public class Snake extends LadderSnake{
+    // always puts the player 4 squares backwards
     public Snake(int start_square) {
         super(start_square, start_square -4);
         }
