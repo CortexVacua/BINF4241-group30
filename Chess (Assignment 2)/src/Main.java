287163e7c039489ade26584f8a124d874d18f76b
CortexVacua <45538598+CortexVacua@users.noreply.github.com>
2019-10-19 14:56:47 +0200

    Added initialization for the pieces in Gameboard

--- Chess (Assignment 2)/src/Main.java
+++ Chess (Assignment 2)/src/Main.java
@@ -1,14 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         Gameboard gb1 = new Gameboard();
-        for (int i = 0; i<=63; i++) {
-            Field x = gb1.Fields.get(i);
-            x.getaColumn().get_column_number();
+//        for (int i = 0; i<=63; i++) {
+//            Field x = gb1.Fields.get(i);
+//            x.getaColumn().get_column_number();
 //            System.out.println(x.getaColor());
 //            System.out.println(x.getaRow());
 //            System.out.println(x.getaColumn());
 //            System.out.println(x.getaOccupied());
 //            System.out.println("\n\n");
+        for (int i=0; i<32; i++) {
+            Piece x = gb1.Pieces.get(i);
+            System.out.println(x.getClass());
+            System.out.println(x.getColor());
+            System.out.println(x.getX());
+            System.out.println(x.getY());
+            System.out.println(x.getNumber_of_moves());
+            System.out.println("\n\n");
         }
     }
 }
