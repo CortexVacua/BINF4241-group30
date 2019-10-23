ca822ebbde8fe1a8588fd4a5310ca3d2d804b976
bzns <matej.gurica@uzh.ch>
2019-10-24 00:47:41 +0200

    Fixed the isValid method in Piece

--- Chess (Assignment 2)/src/Piece.java
+++ Chess (Assignment 2)/src/Piece.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class Piece {
     protected Row y;
     protected Column x;
@@ -29,30 +31,39 @@ public class Piece {
         }
         //checks if this move undoes check in case of checked King or places the king in check in the first place
         Gameboard gb2 = new Gameboard(gb);
-        for (Piece piece : gb2.getPieces()) {
-          if (piece.getColor() == color && piece instanceof King) {
-                  if (gb2.getPiece(toX, toY) != null) {
-                      for (Piece p : gb2.getPieces()) {
-                          if (p.getRow()== toY && p.getColumn()== toX) {
-                              gb2.Pieces.remove(p);
+        for (int i = 0; i < gb2.getPieces().size(); i++) {
+          if (gb2.getPieces().get(i).getColor() == color && gb2.getPieces().get(i) instanceof King) {
+              if (gb2.getPiece(toX, toY) != null) {
+                  for (int j = 0; j < gb2.getPieces().size(); j++) {
+                      if (gb2.getPieces().get(j).getRow() == toY && gb2.getPieces().get(j).getColumn() == toX) {
+                          gb2.Pieces.remove(j);
+                      }
+                  }
+                  gb2.getPiece(x, y).setPosition(toX, toY);
+                  gb2.getField(x, y).unoccupy();
+                  for (int k = 0; k < gb2.getPieces().size(); k++) {
+                      if (gb2.getPieces().get(k).getColor() == color && gb2.getPieces().get(k) instanceof King) {
+                          if (gb2.getPieces().get(i).checkIfChecked(gb2)) {
+//                              System.out.println("You are making a move that puts or leaves your king in check. ");
+                              return false;
                           }
                       }
-                      gb2.getPiece(x, y).setPosition(toX, toY);
-                      gb2.getField(x, y).unoccupy();
-                      if (((King) piece).checkIfChecked(gb2)) {
-                          System.out.println("You are making a move that puts or leaves your king in check. ");
-                          return false;
+
+                  }
+              } else {
+                  gb2.getPiece(x, y).setPosition(toX, toY);
+                  gb2.getField(x, y).unoccupy();
+                  gb2.getField(toX, toY).occupy();
+                  for (int l = 0; l < gb2.getPieces().size(); l++) {
+                      if (gb2.getPieces().get(l).getColor() == color && gb2.getPieces().get(l) instanceof King) {
+                          if (gb2.getPieces().get(i).checkIfChecked(gb2)) {
+//                              System.out.println("You are making a move that puts or leaves your king in check. ");
+                              return false;
+                          }
                       }
                   }
-                  else {
-                      gb2.getPiece(x, y).setPosition(toX, toY);
-                      gb2.getField(x, y).unoccupy();
-                      if (((King) piece).checkIfChecked(gb2)) {
-                          System.out.println("You are making a move that puts or leaves your king in check. ");
-                          return false;
-                      }
-                  }
-                }
+              }
+          }
         }
         return toX.column_number != x.column_number || toY.row_number != y.row_number;
     }
@@ -91,5 +102,8 @@ public class Piece {
         this.y = y;
         this.x = x;
         number_of_moves ++;
+    }
+    public boolean checkIfChecked(Gameboard gb) {
+        return false;
     }
 }
