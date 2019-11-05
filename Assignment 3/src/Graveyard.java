dccd4ac24569b8ab5b824f08f760e463edf10530
MinhFu <45556687+MinhFu@users.noreply.github.com>
2019-11-05 02:14:38 +0100

    Implemented a Graveyard for our fallen Pieces

---
+++ Assignment 3/src/Graveyard.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graveyard implements ObserverPieces {
+
+    private List<Piece> Dead_White_Pieces = new ArrayList<>();
+    private List<Piece> Dead_Black_Pieces = new ArrayList<>();
+
+    public List<Piece> getDead_White_Pieces() {
+        return  Dead_White_Pieces;
+    }
+
+    public List<Piece> getDead_Black_Pieces() {
+        return  Dead_Black_Pieces;
+    }
+
+    public void update(Piece dead_piece) {
+        if (dead_piece.getColor() == Color.WHITE) Dead_White_Pieces.add(dead_piece);
+        else Dead_Black_Pieces.add(dead_piece);
+    }
+}
